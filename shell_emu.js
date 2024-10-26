d37dea65c9cd95f33bea51d0d8b1516dc22f3de3
Tito Hinostroza <tehh2@DESKTOP-IQ840EC>
2024-10-26 13:26:12 -0500

    Comando "ls" básico

--- shell_emu.js
+++ shell_emu.js
@@ -9,6 +9,8 @@ function TitoShellEmul(response) {
     let user_list = [];
     let log_user;
     let log_pass;
+    let cur_path = "";
+    let filesystem = [];    //Arreglo para el sistema de archivos
     //Modos de trabajo del Shell
     const SM_LOGIN = 0;     //Modo esperando usuario
     const SM_PASSW = 1;     //Modo esperando contraseña
@@ -72,7 +74,12 @@ function TitoShellEmul(response) {
             if (curCommand=='date') {
                 let cur_date = new Date().toGMTString(); 
                 response(cur_date + "\n")
-            } else {
+            } else if (curCommand=='pwd') {
+                response(cur_path + "\n")
+            } else if (curCommand=='ls') {
+                //response(cur_path + "\n")
+                do_ls();
+            } else{
                 response(curCommand + ": Command not found\n")
             }
             curCommand = '';
@@ -123,6 +130,63 @@ function TitoShellEmul(response) {
             shell.writeCode(keyCode);
         }
     }
+    function do_ls() {
+        /**Hace un listado del directorio actual */
+        if (cur_path=="/") {
+            let cur_dir = filesystem;
+            //Lista el directorio "cur_dir"
+            let claves = Object.keys(cur_dir);  //Para poder iterar sobre los archivos
+            if (claves.length==0) return;   //No hay archivos
+            let maxlen = 0;    //Tamaño máximo de los nombres de lo archivos
+            let totlen = 0;    //Tamaño total del nombre de los archivos 
+            for (let f_name in cur_dir) {   //Calcula el tamaño máximo de los nombres
+                totlen += f_name.length + 2;
+                if (f_name.length>maxlen) maxlen = f_name.length;
+            }
+            maxlen += 2;    //Aumenta dos caracteres para dejar espacio
+            totlen -= 2;    //Corrige quitando espacio al último
+            let ncols = Math.floor(80/maxlen);  //Calcula número de columnas para el listado
+            let nlins = Math.ceil(claves.length/ncols);  //Filas por columna
+            if (ncols>=claves.length || totlen<=80) {    //Se puede listar en una sola fila
+                for (let f_name in cur_dir) {
+                    response(f_name + "  ");
+                }            
+                response("\n")
+            } else {    //Se debe listar en varias filas
+                //response("ncols=" + ncols + "\n");
+                //response("nlins=" + nlins + "\n");
+                //response("maxlen=" + maxlen + "\n");
+                //Lista por filas
+                for (let fil = 0; fil < nlins; fil++) {
+                    //Escribe por columnas
+                    for (let col = 0; col < ncols; col++) {
+                        let i = col*nlins+fil;
+                        if (i<claves.length) {
+                            let f_name = claves[i].padEnd(maxlen);
+                            response(f_name);
+                        }
+                    }
+                    response("\n")
+                }
+            }
+        }
+    }
+    function init_filesystem() {
+        //filesystem = new Array(NROWS);  
+        //for(var i=0; i<screen.length; i++) {
+        //    screen[i] = new Array(NCOLS);
+        //}
+        filesystem["bin"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["dev"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["lib"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["usr123456789012"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["camino"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["camino123"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["nombre_algo_largo"]  = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["root"] = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        filesystem["_hola"] = ['lrwxrwxrwx', "Hola mundo"];  //"Hola mundo";
+        cur_path = "/";
+    }
     function init() {
         /* Inicializa el emulador del Sistema Operativo e inicia el modo de inicio de
          sesión */
@@ -132,7 +196,7 @@ function TitoShellEmul(response) {
         //Mensaje de saludo
         response("\n");
         response("Titux OS System. Javascript Linux Emulator.\n");
-        response("Kernel 0.0.1-Educational-version.\n");
+        response("Kernel 0.0.2-Educational-version.\n");
         response("\n");
         ////////////// Prueba de control de pantalla //////////////
 //        response("\x1B[5;5fTexto en (5,5)");
@@ -165,25 +229,6 @@ function TitoShellEmul(response) {
 //        response("\x1B[0;46mTexto con Fondo cian\n");
 //        response("\x1B[0;47mTexto con Fondo blanco\n");
 //
-//        response("\x1B[0;49mTexto con Fondo normal\n");
-//
-//        response("\x1B[38;2;190;130;0mTexto con color RGB\n");
-//
-//        response("\x1B[38;5;11mTexto con color de 8 bits\n");
-//        response("\x1B[48;5;3mTexto con fondo de 8 bits\n");
-//        response("\x1B[0m\x1B[38;5;21mTexto con color RGB de 8 bits\n");  //Azul
-//        response("\x1B[0m\x1B[38;5;46mTexto con color RGB de 8 bits\n");  //Verde
-//        response("\x1B[0m\x1B[38;5;196mTexto con color RGB de 8 bits\n");  //Rojo
-//
-//        response("\x1B[0m\x1B[38;5;255m");  //Prepara para escala de grises
-//        response("\x1B[48;5;232mTexto con gris de 8 bits\n");
-//        response("\x1B[48;5;236mTexto con gris de 8 bits\n");
-//        response("\x1B[48;5;240mTexto con gris de 8 bits\n");
-//        response("\x1B[48;5;244mTexto con gris de 8 bits\n");
-//        response("\x1B[48;5;248mTexto con gris de 8 bits\n");
-//        response("\x1B[48;5;252mTexto con gris de 8 bits\n");
-
-
 //        response("\n");
 //        #Paleta de colores
 //        for((i=16; i<256; i++)); 
@@ -193,10 +238,11 @@ function TitoShellEmul(response) {
 //        done
 
         //////////////////////////////////////////////////////////
+        init_filesystem();
         modeShell = SM_LOGIN;   //Modo de inicio de sesión
+//        modeShell = SM_COMMAND;   //Modo de comandos
         response("\x1B[0m");    //Restaura atributos
         response('Login: ');
-
     }
     //Métodos públicos
     this.write = function(str) {write(str)};
