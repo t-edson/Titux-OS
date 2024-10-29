49f729ef2cf44b38871597d0af2564b0a29a00ae
Tito Hinostroza <dev_null_1@hotmail.com>
2024-10-29 12:42:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 Pure Javascript linux shell emulator
 
+Titux-OS is a Javascript library that implements a Linux shell emulator, intended to run completely in any Web browser.
+
+This emulator is written entirely in Javascript and runs offline.
+
+At the moment, only the login and file listing commands are implemented.
+
+The goal is not to have a complete Linux, but rather a shell emulator that allows you to 
+perform basic operations, without having to be connected to a real server and without 
+worrying about breaking something important.  In addition, there are no remote connection 
+problems, because everything runs on the same client PC.
+
+The idea is that it can be used as an educational tool. New custom commands can be added relatively easily.
+
+This project is in the process of implementation, still in an early stage.
+
+## FILES 
+
+This library is made up of 3 files:
+
+* titoansi.js
+* titoterm.js
+* shell_emu.js
+
+All three should be included on the website. The first two implement the terminal with support for ANSI sequences. These files have been copied from the project 
+
+The shell emulator, he completed, is in the shell_emu.js file.
+
--- shell_emu.js
+++ shell_emu.js
@@ -6,11 +6,14 @@ function TitoShellEmul(response) {
     let curXpos = 0;        //Posición actual del cursor con respecto al comando actual
     let modeShell = 0;      //Modo de trabajo del shell
     let prompt = '$ ';
-    let user_list = [];
-    let log_user;
-    let log_pass;
-    let cur_path = "";
+    let user_list = [];     //Lista de usuarios
+    let group_list = [];     //Lista de grupos
+    let log_user  = '';     //Usuario actual
+    let log_pass  = '';
+    let log_group = '';     //Grupo actual
+    let cur_path  = '';     //Directorio actual
     let filesystem = [];    //Arreglo para el sistema de archivos
+    let err = "";           //Mensaje de error
     //Modos de trabajo del Shell
     const SM_LOGIN = 0;     //Modo esperando usuario
     const SM_PASSW = 1;     //Modo esperando contraseña
@@ -56,12 +59,12 @@ function TitoShellEmul(response) {
         } else if (modeShell==SM_PASSW) {
             //Valida la contraseña
             log_pass = curCommand; 
-            let pwd = user_list[log_user];
-            if (log_user in user_list && log_pass==pwd) {
+            if (log_user in user_list && log_pass==user_list[log_user][0]) {
                 //Pasa a modo comando
+                modeShell=SM_COMMAND;
+                log_group = user_list[log_user][1];     //Lee grupo
                 let last_login = new Date().toGMTString(); 
                 response('Last login: ' + last_login + " from tty1\n");
-                modeShell=SM_COMMAND;
                 response(prompt);
             } else {
                 response("Login incorrect.\n\n");
@@ -71,14 +74,15 @@ function TitoShellEmul(response) {
             curCommand = '';
             curXpos = 0;
         } else if (modeShell==SM_COMMAND) {
-            let parts = curCommand.split(' ');
-            if (parts[0]=='') {
-            } else if (parts[0]=='date') {
+            let parts = curCommand.split(/\s+/);
+            let command = parts[0];
+            if (command =='') {
+            } else if (command=='date') {
                 let cur_date = new Date().toGMTString(); 
                 response(cur_date + "\n")
-            } else if (parts[0]=='pwd') {
+            } else if (command=='pwd') {
                 response(cur_path + "\n")
-            } else if (parts[0]=='ls') {
+            } else if (command=='ls') {
                 //response(cur_path + "\n")
                 do_ls(parts);
             } else{
@@ -132,6 +136,29 @@ function TitoShellEmul(response) {
             shell.writeCode(keyCode);
         }
     }
+    function expandFiles(fname, fdir) {
+        /*Expande un nombre de archivo con comodines (fname) en una lista de archivos, 
+        de acuerdo a la lista de archivos que existan en la lista "fdir".
+        Devuelve la lista de archivos en una lista  con los archivos que cumplen el 
+        patrón.*/
+        let flist = {};
+        if (/[\*\?]/.test(fname)) { //Contiene caracteres comodín
+            //Hay que expandir
+            const regex = new RegExp(fname.replace(/\?/g, '.').replace(/\*/g, '.*')); // Convierte comodines a una expresión regular
+            for (const clave in fdir) {
+                if (regex.test(clave)) { // Verificar si la clave coincide con el patrón
+                    flist[clave] = fdir[clave]; // Copiar al nuevo diccionario
+                }
+            }            
+        } else {    //No contiene caracteres comodín
+            for (const clave in fdir) {
+                if (clave==fname) { // Verificar si existe
+                    flist[clave] = fdir[clave]; // Copiar al nuevo diccionario
+                }
+            }            
+        }
+        return flist;
+    }
     function do_ls(parts) {
         /**Hace un listado del directorio actual */
         function list_columns(fnames, maxlen, totlen) {
@@ -161,60 +188,108 @@ function TitoShellEmul(response) {
             }
         }
         function list_details(cur_dir) {
-//                for (let i = 0; i < cur_dir.length; i++) {
-//                    response(cur_dir[i] + "\n");
-//                }
+                const opc = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false };
+                const fecActual = new Date();
                 for (let key in cur_dir) {
                     let fdat = cur_dir[key];
+                    //Da formato a fecha
+                    let fmod = fdat[5];
+                    const fecFormat = fmod.toLocaleString('en-US', opc);   //"Mar 07, 2023, 03:22"
+                    const parts = fecFormat.split(', ');
+                    let fecShort;
+                    if (fmod.getFullYear() === fecActual.getFullYear()) {
+                        fecShort = parts[0] + ' ' + parts[2];
+                    } else {
+                        fecShort = parts[0] + '  ' + parts[1];
+                    };
                     response(fdat[0] + " " + 
                         String(fdat[1]).padStart(3) + " "  +  //Permisos
                         fdat[2].padEnd(8) + " " +       //Usuario
                         fdat[3].padEnd(8) + " " +       //Grupo
                         String(fdat[4]).padStart(6) + " " +   //Tamaño
+                        fecShort + " " +   //Fecha
                         key +  "\n");
                 }                
         }
-        if (cur_path=="/") {
-            let cur_dir = filesystem;
-            //Lista el directorio "cur_dir"
-            let fnames = Object.keys(cur_dir);  //Para poder iterar sobre los archivos
-            if (fnames.length==0) return;   //No hay archivos
-            let maxlen = 0;    //Tamaño máximo de los nombres de lo archivos
-            let totlen = 0;    //Tamaño total del nombre de los archivos 
-            for (let f_name in cur_dir) {   //Calcula el tamaño máximo de los nombres
-                totlen += f_name.length + 2;
-                if (f_name.length>maxlen) maxlen = f_name.length;
+        let cur_dir;
+        //Busca si hay nombre de archivo
+        let fname = '';
+        for (let i = 1; i < parts.length; i++) {
+            let par = parts[i].trim();  //Parámetro
+            if (par[0]!='-') fname = par;
+        }
+        //Filtra lista de archivos por nombre
+        if (fname=='') {
+            cur_dir = do_getdir(cur_path);  //Todo el directorio
+        } else {
+            //Hay que buscar los que cumplan
+            /*Por simplicidad, aquí asumimos que "fname" no contiene información del
+            directorio, sino tan solo el nombre. */
+            let fdir = do_getdir(cur_path);  //Directorio actual
+            cur_dir = expandFiles(fname, fdir);   //Archivos que cumplen
+        }
+        //Lista el directorio "cur_dir"
+        let maxlen = 0;    //Tamaño máximo de los nombres de los archivos
+        let totlen = 0;    //Tamaño total del nombre de los archivos 
+        for (let f_name in cur_dir) {   //Calcula el tamaño máximo de los nombres
+            totlen += f_name.length + 2;
+            if (f_name.length>maxlen) maxlen = f_name.length;
+        }
+        maxlen += 2;        //Aumenta dos caracteres para dejar espacio
+        totlen -= 2;        //Corrige quitando espacio al último
+        let fnames = Object.keys(cur_dir);  //Para poder iterar sobre los archivos
+        if (fnames.length==0) return;   //No hay archivos
+        if (parts.includes('-l')) list_details(cur_dir); 
+        else list_columns(fnames, maxlen, totlen);
+    }
+    function do_getdir(path) {
+        /* Devuelve el directorio "path" en una lista o diccionario. */
+        if (path=="/") {    //Directorio raiz
+            return filesystem;
+        } else { //Hay que buscar el directorio
+            let cur_dir = {};
+            if (path[0] == '/') {   //Ruta absoluta
+                let parts = path.split('/');
+                cur_dir = filesystem;   //DIrectorio inicial
+                for (let i = 0; i < parts.length; i++) {
+                    if (parts[i] in cur_dir) {
+                        cur_dir = cur_dir[6];  //Toma la lista de directorio
+                    } else {
+                        err = 'No such file or directory';
+                        return cur_dir;
+                    }
+                }
+            } else {    //Ruta relativa
+                err = 'Invalid path';
+                ////// Completar
             }
-            maxlen += 2;    //Aumenta dos caracteres para dejar espacio
-            totlen -= 2;    //Corrige quitando espacio al último
-            list_columns(fnames, maxlen, totlen);
-            //list_details(cur_dir);
         }
     }
+    function do_mkdir(dir_name) {
+        /* Crea un nuevo directorio en la carpeta actual.*/
+        let cur_dir = do_getdir(cur_path);
+    }
     function init_filesystem() {
-        //filesystem = new Array(NROWS);  
-        //for(var i=0; i<screen.length; i++) {
-        //    screen[i] = new Array(NCOLS);
-        //}
         /* Una carpeta de archivos es una lista con el nombre del archivo como clave.
         Los archivos se definen como una lista con los siguientes campos:
-                - Permisos del archico,
-                - Número de enlaces
-                - Propietario del archivo
-                - Grupo al que pertenece
-                - Tamaño del archivo en bytes
-                - Fecha y hora de la última modificación
-                - Contenido del archivo, si se un archivo
+            - Permisos del archico,
+            - Número de enlaces
+            - Propietario del archivo
+            - Grupo al que pertenece
+            - Tamaño del archivo en bytes
+            - Fecha y hora de la última modificación
+            - Contenido del archivo, si es un archivo, o lista de archivos si es una carpeta.
         */
-        filesystem["bin"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), null];
-        filesystem["dev"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), null];  
-        filesystem["lib"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), null];  
-        filesystem["usr123456789012"]   = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
-        filesystem["camino"]            = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
-        filesystem["camino123"]         = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
-        filesystem["nombre_algo_largo"] = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
-        filesystem["root"]              = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
-        filesystem["_hola"]             = ['lrwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/27"), "Hola mundo"];  
+        //filesystem = new Array(NROWS);  
+        filesystem["bin"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/03/07 03:22:00"), null];
+        filesystem["dev"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2023/10/20 01:50:00"), null];  
+        filesystem["lib"]               = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/10/03 04:01:00"), null];  
+        filesystem["usr123456789012"]   = ['-rwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/01/28 01:20:00"), "Hola mundo"];  
+        filesystem["camino.xml"]        = ['-rwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/01/27 05:00:00"), "Hola mundo"];  
+        filesystem["programa"]          = ['-rwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/01/15 01:00:00"), "Hola mundo"];  
+        filesystem["nombre_algo_largo"] = ['-rwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/10/13 01:30:00"), "Hola mundo"];  
+        filesystem["root"]              = ['drwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/04/27 01:20:00"), null];  
+        filesystem["prueba.txt"]        = ['-rwxrwxrwx', 1, 'usuario', 'grupo', 4096, new Date("2024/10/27 01:05:00"), "Hola mundo"];  
         cur_path = "/";
     }
     function init() {
@@ -222,7 +297,8 @@ function TitoShellEmul(response) {
          sesión */
         curCommand = "";
         curXpos = 0;
-        user_list['root'] = 'root'; //Crea al usuario root
+        group_list['grupo'] = 'grupo';  //Crea un grupo
+        user_list['root'] = ['root', 'grupo'];     //Crea al usuario root
         //Mensaje de saludo
         response("\n");
         response("Titux OS System. Javascript Linux Emulator.\n");
@@ -250,27 +326,13 @@ function TitoShellEmul(response) {
 //
 //        response("\x1B[0;39mTexto en color normal\n");
 //
-//        response("\x1B[0;40mTexto con Fondo negro\n");
-//        response("\x1B[0;41mTexto con Fondo rojo\n");
-//        response("\x1B[0;42mTexto con Fondo verde\n");
-//        response("\x1B[0;43mTexto con Fondo amarillo\n");
-//        response("\x1B[0;44mTexto con Fondo azul\n");
-//        response("\x1B[0;45mTexto con Fondo magenta\n");
-//        response("\x1B[0;46mTexto con Fondo cian\n");
-//        response("\x1B[0;47mTexto con Fondo blanco\n");
-//
-//        response("\n");
-//        #Paleta de colores
-//        for((i=16; i<256; i++)); 
-//        do printf "\e[48;5;${i}m%03d" $i; 
-//        printf '\e[0m'; 
-//        [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n' 
-//        done
-
         //////////////////////////////////////////////////////////
         init_filesystem();
         modeShell = SM_LOGIN;   //Modo de inicio de sesión
 //        modeShell = SM_COMMAND;   //Modo de comandos
+//        log_user = 'root';
+//        log_group = 'grupo';
+
         response("\x1B[0m");    //Restaura atributos
         response('Login: ');
     }
